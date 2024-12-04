371f3eaf29fa114135c4b29311e4441ee1048a60
Quan Huynh <quan.m.huynh@edu.sait.ca>
2024-12-03 19:56:19 -0700

    try fix login sizing

--- app/page.js
+++ app/page.js
@@ -26,17 +26,15 @@ export default function LoginPage() {
         <main className="bg-slate-900 w-full h-screen flex justify-center items-center">
             <section className=" bg-gray-800 w-1/3 h-3/4 rounded-lg">
                 <section className="m-10 flex flex-col items-center">
-                    {/* Icon from
-          https://www.flaticon.com/free-icon/game-controller_141416 */}
+                    {/* Icon from https://www.flaticon.com/free-icon/game-controller_141416 */}
                     <img className=" w-1/6 h-auto filter grayscale invert" src="/images/game-controller.png"></img>
-                    {/* <h1 className="text-4xl font-bold">My Game List</h1> */}
-                    <img className="border-b border-white" src="/images/my-game-list.png"></img>
+                    <img className="w-full border-b border-white" src="/images/my-game-list.png"></img>
                 </section>
 
                 {!user && (
                     <section className="flex flex-col justify-center items-center" onClick={() => handleGitHubSignIn()} >
                         <h2 className="text-2xl mb-5">Login</h2>
-                        <section className="bg-neutral-700 w-60 flex p-4 rounded-lg hover:bg-neutral-600 focus:bg-neutral-700">
+                        <section className="bg-neutral-700 w-3/5 flex p-4 justify-center rounded-lg hover:bg-neutral-600 focus:bg-neutral-700">
                             <img className="w-10 h-auto mr-5 filter grayscale invert" src="/images/github-logo.png"></img>
                             <button type="button">Log in With GitHub</button>
                         </section>
